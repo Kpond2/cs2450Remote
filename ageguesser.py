a62985c3f23d9df33d22bbd38036420bc4d95348
Kai Pond <kaipond@Kais-MacBook-Pro.local>
2025-08-28 17:39:24 -0600

    finally got the program running as expected

--- ageguesser.py
+++ ageguesser.py
@@ -1,19 +1,18 @@
 import random
 
 def age_guesser():
-    print("Hello, This is the age guesser. I will attempt to guess your age.")
-    name = input("What is your name?")
+    print("Hello! This is the age guesser. I will attempt to guess your age.")
+    name = input("What is your name? ")
 
-    age = random.randint(15, 30)
+    while True:
+        age = random.randint(15, 30)
+        print("I guess your age is", age)
+        answer = input("Is that correct? (y/n) ").lower()  
+        if answer == "y":
+            print(name + " is " + str(age) + " years old!")
+            break
+        else:
+            print("Rats.")
 
-    print("I guess your age is", age)
-    answer = input("Is that correct? (y/n)")
 
-    if answer == "y":
-        print(name + " is " + age + " years old.")
-    else:
-        print("Rats.")
-        print("I guess your age is", str(age)
-        answer = input("Is that correct? (y/n)")
-        print("Rats")
 age_guesser()
